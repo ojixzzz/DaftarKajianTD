8567f94ba09434a033b67a970f1e09313723bbac
Adaramuju <adaramuju@gmail.com>
2021-09-25 08:41:57 +0700

    logo td

--- flaskr/static/js/main.js
+++ flaskr/static/js/main.js
@@ -8,7 +8,7 @@ $(function(){
         stepsOrientation: "vertical",
         autoFocus: true,
         transitionEffectSpeed: 500,
-        titleTemplate : '<div class="title">#title#</div>',
+        titleTemplate : '<div class="title" style="padding: 20px 25px 15px 25px;">#title#</div>',
         onStepChanging: function (event, currentIndex, newIndex)
         {
             $("#form-register").validate().settings.ignore = ":disabled,:hidden";
